f1e363d0067e791c74518ec898d30879e0f1e2e6
Mark Xu <mark.xu@itsp-inc.com>
2018-09-06 18:55:58 -0400

    404 route

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,11 +4,13 @@ import {RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from "./home.component";
+import { NotFoundComponent } from "./not-found.component";
 import {FormsModule} from "@angular/forms";
 import {PassengerDashboardModule} from "./passenger-dashboard/passenger-dashboard.module";
 
 const routes: Routes = [
-    { path: '', component: HomeComponent, pathMatch: 'full'}
+    { path: '', component: HomeComponent, pathMatch: 'full'},
+    { path: '**', component: NotFoundComponent}
 ]
 @NgModule({
   imports: [
@@ -25,6 +27,7 @@ const routes: Routes = [
   declarations: [
     AppComponent,
       HomeComponent,
+      NotFoundComponent
   ]
 })
 export class AppModule {}
